a49e4fcec1bac4a121f0f75e98b9b24257f6d169
winstr <seunghyeon.me@gmail.com>
2023-09-21 08:56:00 +0000

    refactoring

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -1,139 +1,122 @@
 import os
 import time
 import pickle
+from tqdm import tqdm
 
 import cv2
 import numpy as np
 
 
-class FailedToOpenError(RuntimeError): pass
-class FailedToReadError(RuntimeError): pass
+class FailedToOpenError(RuntimeError):
+    pass
 
 
-def save_data(filepath, data):
-    with open(filepath, "wb") as f:
-        pickle.dump(data, f)
+class FailedToReadError(RuntimeError):
+    pass
 
 
-def load_data(filepath):
-    with open(filepath, "rb") as f:
-        data = pickle.load(f)
-    return data
+class Callibrator():
+
+    def __init__(self, chessboard, square_size) -> None:
+        self.chessboard = chessboard
+
+        self.objp = np.zeros((1, chessboard[0]*chessboard[1], 3), np.float32)
+        self.objp[0, :, :2] = \
+            np.mgrid[0:chessboard[0],
+                     0:chessboard[1]].T.reshape(-1, 2) * square_size
+        self.objpoints = []
+        self.imgpoints = []
+
+        self.calibration_data = {
+            "ret": None,
+            "mtx": None,
+            "dist": None,
+            "rvecs": None,
+            "tvecs": None}
+
+    def calibrate(self, image_paths, image_width, image_height) -> None:
+        for path in tqdm(image_paths, total=len(image_paths)):
+            image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+            if image is None:
+                raise RuntimeError(path)
+            ret, corners = cv2.findChessboardCorners(
+                image,
+                self.chessboard,
+                cv2.CALIB_CB_ADAPTIVE_THRESH
+                + cv2.CALIB_CB_FAST_CHECK
+                + cv2.CALIB_CB_NORMALIZE_IMAGE)
+            if ret:
+                self.objpoints.append(self.objp)
+                self.imgpoints.append(corners)
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+            self.objpoints, self.imgpoints, (image_width, image_height), None, None)
+        self.calibration_data["ret"] = ret
+        self.calibration_data["mtx"] = mtx
+        self.calibration_data["dist"] = dist
+        self.calibration_data["rvecs"] = rvecs
+        self.calibration_data["tvecs"] = tvecs
+
+    def get_calibration_data(self) -> dict:
+        return self.calibration_data
 
 
-class CameraViewer():
+class VideoViewer():
 
-    def __new__(cls, *args, **kwargs):
-        raise TypeError("This class cannot be instantiated.")
-
-    def __init__(self, device_file) -> None:
-        if not os.path.isfile(device_file):
-            raise FileNotFoundError(device_file)
-        self.cap = cv2.VideoCapture(device_file)
+    def __init__(self, source) -> None:
+        self.cap = cv2.VideoCapture(source)
         if not self.cap.isOpened():
-            raise FailedToOpenError(device_file)
-        self.is_calibrated = False
+            raise FailedToOpenError(source)
 
-    def __del__(self) -> None:
-        self.cap.release()
+        self.is_streaming_source = self.cap.get(
+            cv2.CAP_PROP_FRAME_COUNT) <= 0
 
-    def set_configs(self, width, height, exposure, cvt_rgb=False) -> None:
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        self.cap.set(cv2.CAP_PROP_EXPOSURE, exposure)
-        self.cap.set(cv2.CAP_PROP_CONVERT_RGB, cvt_rgb)
-
-    def _undistort(self, image) -> np.ndarray:
-        # When overriding this function in a subclass, ensure to set
-        # `self.is_calibrated` to `True` if calibration is done.
-        raise NotImplementedError()
-
-    def _flatten(self, image) -> np.ndarray:
-        raise NotImplementedError()
-
-    def grab_image(self) -> np.ndarray:
+    def read(self) -> np.ndarray:
         retval, image = self.cap.read()
         if not retval:
-            raise FailedToReadError()
-        if self.is_calibrated:
-            image = self._undistort(image)
+            raise FailedToOpenError()
         return image
 
-    def view_image(self) -> None:
-        winname = f"[Preview] cap_id:{id(self.cap)}"
-        image = self.grab_image()
-        image = self._flatten(image)
-        cv2.imshow(winname, image)
-        cv2.waitKey(0)
-        cv2.DestroyWindow(winname)
-
-    def view_video(self) -> None:
-        winname = f"[Preview] cap_id:{id(self.cap)}"
-        prev_time = 0
-        curr_time = 0
+    def show(self) -> None:
+        winname = f"{id(self.cap)}"
+        if self.is_streaming_source:
+            wait_ms = 1  # millisecond
+        else:
+            fps = self.cap.get(cv2.CAP_PROP_FPS)
+            wait_ms = int(1000.0 / fps)
         while True:
-            image = self.grab_image()
-            image = self._flatten(image)
-            curr_time = time.time()
-            fps = 1 / (curr_time - prev_time)
-            cv2.putText(
-                image=image,
-                text=f"{str(fps):.2f}",
-                org=(50, 100),
-                font=cv2.FONT_HERSHEY_PLAIN,
-                fontscale=1.0,
-                color=(0, 255, 0),
-                lineType=cv2.Line_AA)
+            image = self.read()
             cv2.imshow(winname, image)
-            if cv2.waitKey(1) == ord("q"):
+            key = cv2.waitKey(wait_ms)
+            if key == ord("q"):
                 break
-            prev_time = curr_time
-        cv2.destroyWindow(winname)
-
-    def save_image(self) -> None:
-        raise NotImplementedError()
-    
-    def save_video(self) -> None:
-        raise NotImplementedError()
-
-    """
-    def save_video(self, fps, fourcc, filename) -> None:
-        if os.path.isfile(filename):
-            raise FileExistsError(filename)
-        fourcc = cv2.VideoWriter_fourcc(*fourcc)
-        frame_size = (int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                      int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-        writer = cv2.VideoWriter(filename, fourcc, fps, frame_size)
-        winname = f"[Preview] cap_id:{id(self.cap)}, fps:{fps}"
-        while True:
-            image = self.grab_image()
-            cv2.imshow(winname, image)
-            if cv2.waitKey(1) == ord("q"):
-                break
-            writer.write(image)
-        cv2.destroyWindow(winname)
-        writer.release()
-    """
 
 
-class oCamS_1CGN_U(CameraViewer):
+class CameraViewer(VideoViewer):
 
-    def __init__(self, device_file="/dev/cam/oCamS-1CGN-U") -> None:
+    def __init__(self, camera_source) -> None:
+        super().__init__(camera_source)
+        self.mtx = None
+        self.dist = None
+
+    def set_parameters(self, mtx, dist) -> None:
+        self.mtx = mtx
+        self.dist = dist
+
+    def read(self, is_calibrated=False) -> np.ndarray:
+        image = super().read()
+        if is_calibrated:
+            image = cv2.undistort(image, self.mtx, self.dist)
+
+
+class oCamS_1CGN_U(VideoViewer):
+
+    def __init__(self, device_file) -> None:
         super().__init__(device_file)
-        self.set_configs(
-            width=640,
-            height=480,
-            exposure=200,
-            cvt_rgb=False)
-    
-    def set_calibration(self, mtx_L, dist_L, mtx_R, dist_R):
-        super().set_calibration(mtx=(mtx_L, mtx_R), dist=(dist_L, dist_R))
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.cap.set(cv2.CAP_PROP_EXPOSURE, 200)
 
-    def get_frame(self):
-        ret, frame = self.cam.read()
-        if not ret:
-            raise CameraOpenError()
-        if self.is_calibrated:
-            frame[:,:,0] = cv2.undistort(frame[:,:,0], self.mtx[1], self.dist[1])  # right
-            frame[:,:,1] = cv2.undistort(frame[:,:,1], self.mtx[0], self.dist[0])  # left
-        return frame
+
+if __name__ == "__main__":
+    stereo_cam = oCamS_1CGN_U()
+    stereo_cam.show()
