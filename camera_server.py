f04a70eea68188189c44c59ee036f39a6b9622ef
winstr <seunghyeon.me@gmail.com>
2023-10-27 03:48:50 +0900

    added camera streaming server code

---
+++ camera_server.py
@@ -0,0 +1,14 @@
+import socket
+import pickle
+import struct
+
+import cv2
+
+
+def send_frames(client_socket: socket,
+                cap: cv2.VideoCapture):
+    pass
+
+
+if __name__ == '__main__':
+    pass
