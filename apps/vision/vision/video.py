d0cefd4cef448e76b9b2a1017a47b4eb5c0ea24c
seunghyeon <seunghyeon.me@gmail.com>
2024-03-05 07:13:20 +0000

    restore location and added lines to fix the blocking of the read() method.

--- apps/vision/vision/video.py
+++ apps/vision/vision/video.py
@@ -1,27 +1,27 @@
 import threading
 import traceback
-from queue import Queue
-from typing import Union
+from queue import Queue, Full, Empty
 
 import cv2
 import numpy as np
 
 
-# --- Exceptions ---
-class ConnectionError(RuntimeError):
+class VideoCaptureConnectionError(RuntimeError):
+
     def __init__(self, source: str):
-        super().__init__(f'Failed to open {source}.')
+        super().__init__(f'Failed to open the source {source}.')
 
 
-class GrabError(RuntimeError):
+class VideoCaptureGrabError(RuntimeError):
+
     def __init__(self):
         super().__init__('Failed to grab the next frame.')
 
 
-class RetrieveError(RuntimeError):
+class VideoCaptureRetrieveError(RuntimeError):
+
     def __init__(self):
         super().__init__('Failed to retrieve the next frame.')
-# ---
 
 
 class VideoCaptureThread(threading.Thread):
@@ -30,45 +30,58 @@ class VideoCaptureThread(threading.Thread):
         super().__init__()
         self._video_source = video_source
         self._frame_buffer = Queue(maxsize=1)
+        self._frame_spared = np.zeros(shape=(640, 360), dtype=np.uint8)
 
-        self._capture_event = threading.Event()
+        self._event = threading.Event()
         self._stop_capturing = False
 
     def run(self):
         cap = cv2.VideoCapture(self._video_source)
         if not cap.isOpened():
-            raise ConnectionError(self._video_source)
-        self._capture_event.set()
+            raise VideoCaptureConnectionError(self._video_source)
+
+        self._event.set()
 
         try:
             while not self._stop_capturing:
-                self._capture_event.wait()
+                self._event.wait()
 
                 is_grabbed = cap.grab()
                 if not is_grabbed:
-                    raise GrabError()
+                    raise VideoCaptureGrabError()
 
                 is_captured, frame = cap.retrieve()
                 if not is_captured:
-                    raise RetrieveError()
+                    raise VideoCaptureRetrieveError()
 
-                if self._frame_buffer.full():
+                self._frame_spared = frame
+
+                try:
+                    self._frame_buffer.put_nowait(frame)
+                except Full:
                     self._frame_buffer.get()
-                self._frame_buffer.put(frame)
+                    self._frame_buffer.put(frame)
+
         except:
             traceback.print_exc()
+
         finally:
             cap.release()
 
     def pause(self):
-        self._capture_event.clear()
+        self._event.clear()
 
     def resume(self):
-        self._capture_event.set()
+        self._event.set()
 
     def stop(self):
+        if not self._event.is_set():
+            self._event.set()
         self._stop_capturing = True
 
-    def read(self) -> Union[np.ndarray, None]:
-        frame = self._frame_buffer.get()
+    def read(self) -> np.ndarray:
+        try:
+            frame = self._frame_buffer.get_nowait()
+        except Empty:
+            frame = self._frame_spared
         return frame
